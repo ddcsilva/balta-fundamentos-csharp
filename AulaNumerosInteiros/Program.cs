798b5dc9b42fe1d1002b4be9d605aeca33a496ad
Danilo Silva <danilo.silva@msn.com>
2022-01-27 22:46:39 -0300

    Aula sobre Números Inteiros

---
+++ AulaNumerosInteiros/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AulaNumerosInteiros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            short num1 = -1;
+            ushort num2 = 1;
+            int num3 = -1;
+            uint num4 = 1;
+            long num5 = -1;
+            ulong num6 = 1;
+        }
+    }
+}
