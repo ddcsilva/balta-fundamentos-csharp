ad9c97d177265cea736e7fd658e5202148a6faab
Danilo Silva <danilo.silva@msn.com>
2022-01-27 21:36:58 -0300

    Aula Sobre Comentários

---
+++ AulaComentarios/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AulaComentarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // var texto = "Testando";
+
+            /*
+            Comentário de
+            Mútiplas linhas
+            */
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
