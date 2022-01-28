bf42198becf87de10adeea9bda9b3586a666fecf
Danilo Silva <danilo.silva@msn.com>
2022-01-27 21:12:02 -0300

    Aula sobre Variáveis

---
+++ AulaVariaveis/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AulaVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade; // Correto e inicia com ZERO
+            int idade = 25; // Correto e inicia com 25
+            var idade = 25; // Correto e inicia com 25
+            var idade; // Errado
+
+            var texto = "Testando";
+            Console.WriteLine(texto);
+        }
+    }
+}
