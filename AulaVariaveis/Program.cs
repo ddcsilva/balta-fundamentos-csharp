b1bbb1a4669696fdbf61640bdbe5d47868bcfff4
Danilo Silva <danilo.silva@msn.com>
2022-01-28 17:39:06 -0300

    Aula sobre Variáveis - Correção

--- AulaVariaveis/Program.cs
+++ AulaVariaveis/Program.cs
@@ -6,10 +6,10 @@ namespace AulaVariaveis
     {
         static void Main(string[] args)
         {
-            int idade; // Correto e inicia com ZERO
-            int idade = 25; // Correto e inicia com 25
-            var idade = 25; // Correto e inicia com 25
-            var idade; // Errado
+            // int idade; // Correto e inicia com ZERO
+            // int idade = 25; // Correto e inicia com 25
+            // var idade = 25; // Correto e inicia com 25
+            // var idade; // Errado
 
             var texto = "Testando";
             Console.WriteLine(texto);
