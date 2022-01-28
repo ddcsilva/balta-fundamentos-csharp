923d9ba56aa57ec7cbefbe2f7f9916174523f6e8
Danilo Silva <danilo.silva@msn.com>
2022-01-28 18:04:53 -0300

    Aula sobre Var

---
+++ AulaVar/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AulaVar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var idade = 25;
+            // idade = "André"; // ERRADO
+
+            Console.WriteLine(idade);
+        }
+    }
+}
