e41a321e63227524bd54263e50f272e811d43cb2
Danilo Silva <danilo.silva@msn.com>
2022-01-28 17:57:14 -0300

    Aula sobre Strings

---
+++ AulaString/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AulaString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string primeiraLetra = "C";
+            string texto = "Meu texto";
+            var documento = "123456789";
+        }
+    }
+}
