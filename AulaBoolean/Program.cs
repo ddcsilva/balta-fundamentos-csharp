9c1acd982ca0a9738f565ec40be67ddf68388189
Danilo Silva <danilo.silva@msn.com>
2022-01-28 17:50:26 -0300

    Aula sobre Booleanos

---
+++ AulaBoolean/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AulaBoolean
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool usuarioJaCadastrado = false;
+            bool pagamentoRecebido = true;
+            var usuarioExpirado = false;
+
+            System.Console.WriteLine(usuarioJaCadastrado);
+            System.Console.WriteLine(pagamentoRecebido);
+            System.Console.WriteLine(usuarioExpirado);
+        }
+    }
+}
