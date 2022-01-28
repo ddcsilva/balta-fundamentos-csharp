5460050d57b88214b55fa02c577f5fb230543d8f
Danilo Silva <danilo.silva@msn.com>
2022-01-27 21:23:56 -0300

    Aula sobre Constantes

---
+++ AulaConstantes/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AulaConstantes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int IDADE_MINIMA; // Incorreto
+            const int IDADE_MINIMA = 25; // Correto e inicia com 25
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
