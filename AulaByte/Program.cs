ebf7e14af702ad64389571d338a61a99e28dd01b
Danilo Silva <danilo.silva@msn.com>
2022-01-27 22:45:07 -0300

    Aula sobre Bytes

---
+++ AulaByte/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AulaByte
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // byte (8-byte) -> 0 até 255
+            // sbyte (8-byte) -> -128 até 127
+            byte meuByte = 127;
+            sbyte meuByteNegativo = -128;
+        }
+    }
+}
