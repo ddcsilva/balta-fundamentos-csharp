469d73f47f126f3cb39c4df77512cbbc698069a0
Danilo Silva <danilo.silva@msn.com>
2022-01-28 17:53:07 -0300

    Aula sobre Char

---
+++ AulaChar/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AulaChar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char primeiraLetra = 'C';
+            var segundaLetra = 'D';
+
+            System.Console.WriteLine(primeiraLetra);
+            System.Console.WriteLine(segundaLetra);
+        }
+    }
+}
