f4896aa1d0a9e2eacdc17cda699a92b486da5c43
Danilo Silva <danilo.silva@msn.com>
2022-01-28 17:47:00 -0300

    Aula sobre Números Reais

---
+++ AulaNumerosReais/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AulaNumerosReais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double salario = 2.000;
+            float salario2 = 2.500f;
+            decimal salarioAnual = 25.000m;
+
+            System.Console.WriteLine(salario);
+            System.Console.WriteLine(salario2);
+            System.Console.WriteLine(salarioAnual);
+        }
+    }
+}
