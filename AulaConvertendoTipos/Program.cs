627d81583a4fdf10e113dd69a65de76926f504db
Danilo Silva <danilo.silva@msn.com>
2022-01-29 02:37:55 -0300

    Aula sobre Conversões de Tipos

---
+++ AulaConvertendoTipos/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AulaConvertendoTipos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inteiro = 100;
+            float real = 25.5f;
+
+            // inteiro = real; // ERRO
+            // inteiro = (int)real; // FUNCIONA
+            // inteiro = int.Parse(real.ToString()); // ERRO
+            // inteiro = int.Parse("150"); // FUNCIONA
+            // inteiro = Convert.ToInt32(real); // FUNCIONA
+
+            System.Console.WriteLine(inteiro);
+            System.Console.WriteLine(Convert.ToBoolean(1));
+        }
+    }
+}
