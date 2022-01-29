cdd634cd62f03eed1b9f492f8ef0f7ed4c4581b5
Danilo Silva <danilo.silva@msn.com>
2022-01-29 16:54:16 -0300

    Aula sobre a Utilização do IF

---
+++ AulaUtilizandoIf/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AulaUtilizandoIf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade = 16;
+            int maioridade = 18;
+
+            if (idade >= maioridade)
+            {
+                System.Console.WriteLine("É maior de idade");
+            }
+            else
+            {
+                System.Console.WriteLine("É menor de idade");
+            }
+
+            System.Console.WriteLine("Finalizou o programa!");
+        }
+    }
+}
