d0dd820dc316696146d2cb1137496f958183db7b
Danilo Silva <danilo.silva@msn.com>
2022-01-29 00:51:29 -0300

    Aula sobre Object

---
+++ AulaObject/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AulaObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object quantidade;
+            quantidade = 1;
+            quantidade = 2.5;
+            quantidade = "Teste";
+        }
+    }
+}
