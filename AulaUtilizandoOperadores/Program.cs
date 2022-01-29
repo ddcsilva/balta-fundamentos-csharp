7a0aac6ef76058a5495a383878876cb6b647ebd8
Danilo Silva <danilo.silva@msn.com>
2022-01-29 16:44:18 -0300

    Aula sobre a Utilização do IF

---
+++ AulaUtilizandoOperadores/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AulaUtilizandoOperadores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 2;
+            x = 2 / 2;
+
+            Console.WriteLine(x);
+            x++;
+            Console.WriteLine(x);
+            x--;
+
+            System.Console.WriteLine(x < 20);
+            System.Console.WriteLine(x > 20);
+        }
+    }
+}
