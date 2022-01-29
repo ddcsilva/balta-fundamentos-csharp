b1d918b63aaf719285d0479078531d42bdb20d3a
Danilo Silva <danilo.silva@msn.com>
2022-01-29 00:56:03 -0300

    Aula sobre Nullable

---
+++ AulaNullable/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AulaNullable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int? idade = null;
+            System.Console.WriteLine(idade);
+            idade = 0;
+            idade = 25;
+        }
+    }
+}
