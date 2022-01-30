b9fd57de547b847aedcf4ea2ea8a244f62d98b4e
Danilo Silva <danilo.silva@msn.com>
2022-01-30 17:37:00 -0300

    Aula sobre Guid

---
+++ AulaGuid/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AulaGuid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var id = Guid.NewGuid();
+            Console.WriteLine(id);
+
+            id = new Guid("045a0c03-eb69-4152-8b3b-35ee3a1c7d3d");
+            Console.WriteLine(id);
+
+            Console.WriteLine(id.ToString().Substring(0, 8));
+        }
+    }
+}
