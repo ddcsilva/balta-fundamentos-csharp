c7d10b57fb4772695f32e06b981c11a2ee1bb8dd
Danilo Silva <danilo.silva@msn.com>
2022-01-30 11:01:33 -0300

    Aula sobre Switch

---
+++ AulaSwitch/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace AulaSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string valor = "Danilo";
+
+            switch (valor)
+            {
+                case "Danilo":
+                    Console.WriteLine("Encontrou!");
+                    break;
+                case "José":
+                    Console.WriteLine("Errou!");
+                    break;
+                case "João":
+                    Console.WriteLine("Errou!");
+                    break;
+                default:
+                    Console.WriteLine("Incorreto!");
+                    break;
+            }
+        }
+    }
+}
