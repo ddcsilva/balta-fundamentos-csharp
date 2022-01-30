1d5a767bf27c987560f0b93dcbdd0d57b685c227
Danilo Silva <danilo.silva@msn.com>
2022-01-30 17:50:34 -0300

    Aula sobre Comparação de Textos

---
+++ AulaComparacaoStrings/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AulaComparacaoStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var texto = "Testando";
+            Console.WriteLine(texto.CompareTo("Testando"));
+            Console.WriteLine(texto.CompareTo("testando"));
+
+            Console.WriteLine(texto.Contains("Test"));
+            Console.WriteLine(texto.Contains("test"));
+            Console.WriteLine(texto.Contains("test", StringComparison.CurrentCultureIgnoreCase));
+        }
+    }
+}
